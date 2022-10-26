45b5a3fc777a5b74c509336b233d45039105e694
AndrewRiabets <Inferion5@gmail.com>
2022-10-26 20:13:44 +0300

    cli done

--- services/log.service.js
+++ services/log.service.js
@@ -1,22 +1,32 @@
-import chalk from 'chalk'
-import dedent from 'dedent-js'
+import chalk from "chalk";
+import dedent from "dedent-js";
 
 const printError = (err) => {
-    console.log(chalk.bgRed(" ERROR " + ' ' + err))
-}
+  console.log(chalk.bgRed(" ERROR " + " " + err));
+};
 const printSuccess = (err) => {
-    console.log(chalk.bgGreen(" ERROR " + ' ' + err))
-}
+  console.log(chalk.bgGreen(" SUCCESS " + " " + err));
+};
 
 const printHelp = () => {
-    console.log(
-        dedent`${chalk.bgCyan(' HEPL ')} 
+  console.log(
+    dedent`${chalk.bgCyan(" HEPL ")} 
         Без параметров - вывод погоды
         -c [CITY] для установки города
         -h для вывода помощи
         -t [API] для сохоранения токена
         `
-    )
-}
+  );
+};
 
-export {printError, printSuccess, printHelp}
+const printWeather = (res, icon) => {
+  console.log(
+    dedent`${chalk.bgYellow(" WEATHER ")} Погода в городе ${res.name} 
+        ${icon}   ${res.weather[0].description}
+        Температура: ${res.main.temp} (ощущаетася как ${res.main.feels_like})
+        Влажность: ${res.main.humidity}%
+        Скорость ветра: ${res.wind.speed}
+        `
+  );
+};
+export { printError, printSuccess, printHelp, printWeather };
--- weather.js
+++ weather.js
@@ -1,19 +1,72 @@
 #!/ust/bin/env node
-import {getArgs} from './helpers/args.js'
-import { printHelp } from './services/log.service.js'
+import { getArgs } from "./helpers/args.js";
+import { getIcon, getWeather } from "./services/api.service.js";
+import {
+  printHelp,
+  printError,
+  printSuccess,
+  printWeather,
+} from "./services/log.service.js";
+import {
+  getKeyValue,
+  saveKeyValue,
+  TOKEN_DICTIONARY,
+} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Не передан токен");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess("Токен сохранен");
+  } catch (err) {
+    printError(e.message);
+  }
+};
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError("Не передан город");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess("Город сохранен");
+  } catch (err) {
+    printError(e.message);
+  }
+};
+
+const getForcast = async () => {
+  try {
+    const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError("Неверно указан город");
+    } else if (error?.response?.status == 401) {
+      printError("Неверно указан токен");
+    } else {
+      printError(e.message);
+    }
+  }
+};
 
 const initCLI = () => {
-    const args = getArgs(process.argv)
-    if (args.h) {
-        printHelp()
-    }
-    if (args.c) {
-        // city 
-    }
-    if (args.t) {
-        // token
-    }
-    // weather
-}
+  const args = getArgs(process.argv);
+  if (args.h) {
+    return printHelp();
+  }
+  if (args.c) {
+    return saveCity(args.c);
+  }
+  if (args.t) {
+    return saveToken(args.t);
+  }
+  return getForcast();
+};
 
-initCLI()
+initCLI();
